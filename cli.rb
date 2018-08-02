2715ffea6b5f786c46316f7c63bbc5024f248cd4
github username <github email address>
2018-08-02 17:45:14 -0500

    Thurs. 5:45pm. CLI works.

--- cli.rb
+++ cli.rb
@@ -1,5 +1,5 @@
 require 'pry'
-#require_relative 'config/environment.rb'
+require_relative 'config/environment.rb'
 
 def welcome
     puts "Welcome to CJ's 8-ball of lunch options.
@@ -19,13 +19,15 @@ end
 ## If yes, find by name.
 def know_resto
   puts "*" * 45
-  puts "Which restaurant are you interested in?"
+  puts "Which restaurant are you interested in? (Type in the name using proper nouns.)"
   resto = gets.chomp
-  fixed = resto.downcase
+  #fixed = resto.downcase
   ##active record find by.name resto
   puts "Here's your restaurant: "
-  binding.pry
-  Restaurant.find_by(name: fixed)             #case-sensitive???
+  choice = Restaurant.find_by(name: resto)             #case-sensitive???
+    puts choice.name+", rated: "+"#{choice.rating}"+"; great choice! \n Call now: "+choice.phone
+
+  #binding.pry
 end
 
 # binding.pry
@@ -33,28 +35,41 @@ end
 ## If no:
 def get_sorting_method
   puts "*" * 45
-  puts "Here are the restaurants in ½ mi radius from Flatiron. \n
-  (If you want to walk farther than that, \n
+  puts "I'll give you the restaurants in 1/4 mi radius from Flatiron. \n
+  (If you want to walk farther than that,
   clearly you wouldn't be deciding what to eat from a terminal.)
   \n
   Select your sorting method: \n
-  A) Sort by price.
-  B) Sort by rating. (You bougie.) \n
-  Answer 'A' or 'B'."
+  P) Sort by price.
+  R) Sort by rating. (You bougie.) \n
+  Answer 'P' or 'R'."
   # use gets to capture the user's input. This method should return that input, downcased.
   sort_meth = gets.chomp.downcase
 end
 
 def choose_resto(sort_meth)
     puts "*" * 45
-    puts "Here's a list of restaurants near you."
+    puts "Here's a list of 10 restaurants near you."
     sort_meth = sort_meth
 
     # sort by sort_meth (if A, then ... if B, then...)
-    if sort_meth == "a"
-      Restaurant.order(:price)
+    if sort_meth == "p"
+      #binding.pry
+      count = 1
+      Restaurant.order(:price)[0..9].each do |restaurant|
+        puts "#{count}. " + restaurant.name+", with price range: "+ restaurant.price + " and rated: "+"#{restaurant.rating}"+"; #: "+restaurant.phone
+        count += 1
+      end
+
+
     else
-      Restaurant.order(:rating)
+      count = 1
+      Restaurant.order(:rating)[0..9].each do |restaurant|
+        puts "#{count}. " + restaurant.name+", rated: "+"#{restaurant.rating}"+", with price range: "+ restaurant.price + "; #: "+restaurant.phone
+        count += 1
+      end
+
+
     end
 
     # puts "Select a restaurant."
@@ -69,7 +84,7 @@ end
 
 def enjoy
   puts "*" * 45
-  puts "Go enjoy your lunch. Or dinner. Or snack.  Learn, love, code."
+  puts "Go enjoy your lunch. Or dinner. Or snack.  \n Learn, love, code."
 end
 
 def step1
@@ -91,7 +106,7 @@ end
 def step2
   sort_meth = get_sorting_method
 
-  if (sort_meth == 'b'|| sort_meth =='a')
+  if (sort_meth == 'p'|| sort_meth =='r')
       choose_resto(sort_meth)
       know_resto
   else
